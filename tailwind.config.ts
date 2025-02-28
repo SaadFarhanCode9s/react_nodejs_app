d01159c7c42115352afc269840b6227c21c7a86d
Zain-sheikh254 <Zain-sheikh254@gmial.com>
2025-02-28 20:04:16 +0500

    style_sheet

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,8 +11,49 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        bg_green: "#4cab72", 
       },
     },
   },
   plugins: [],
 } satisfies Config;
+
+
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./src/views/**/*.{js,ts,jsx,tsx}",
+//     "./src/views/*.{js,ts,jsx,tsx}",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//     "./node_modules/matrix-react-sdk/src/**/*.{js,ts,jsx,tsx}" //Remember to remove this content once in production phase
+//   ],
+//   important: true,
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         'poppins': ['PoppinsRegular', 'sans-serif'],
+//         'poppins-semibold':['PoppinsSemiBold'],
+//         'poppins-regular':['PoppinsRegular'],
+//         'lato':['Lato'],
+//         'lato-semibold':['LatoSemibold'],
+//       },
+//       colors: {
+//         primaryDark: "#345DEE",
+//         primaryLight: "#162150",
+//         bg_gray: "#13131A",
+//         backgroundBlack: "#14141F",
+//         notificationGreeen: "rgba(61, 214, 170, 0.25)",
+//         filterInactive: "rgba(85, 59, 160, 0.1)"
+//       }
+//     },
+//     screens: {
+//       sm: "640px",
+//       md: "768px",
+//       lg: "1024px",
+//       fd: "1440px",
+//       xl: "1280px",
+//       xxl: "1450px"
+//     }
+//   },
+//   plugins: []
+// };
